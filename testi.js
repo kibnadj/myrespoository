a32c02edd20a6400b951d6848b79e47f8869659c
kibnadj <nadjibkib@gmail.com>
2024-08-18 04:47:13 +0100

    Add files via upload

--- testi.js
+++ testi.js
@@ -2,7 +2,6 @@
 function tran() {
  document.getElementById("tx").innerText="ALLAHOU AKBAR"
  document.getElementById("tx1").innerText="KIBACHE NADJIB BENABDELAZIZ"
-document.getElementById("tx").style.backgroundColor="white";
 document.getElementById("ht").style.backgroundColor="grey";
 }
 
